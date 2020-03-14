a8e21bf0c104371d38fdeb9f46479069947a3d5c
uhareshkumar <uharesh.kumar2015@vit.ac.in>
2020-03-14 21:44:17 +0530

    commit

---
+++ src/launch/launch.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './launch.css'
+
+class launch extends React.Component {
+    render() {
+        return(
+            <div className="container">
+                <div className="align logo">
+
+                </div>
+                <div className="align login">
+
+                </div>
+                <div className="align signup">
+
+                </div>
+            </div>
+        );
+    }
+} 
+
+export default launch;
