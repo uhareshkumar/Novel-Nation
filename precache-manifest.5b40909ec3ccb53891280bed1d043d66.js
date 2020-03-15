974ffd1c232f339e1a7965318aeb9fb79528e37c
uhareshkumar <uharesh.kumar2015@vit.ac.in>
2020-03-15 09:14:04 +0530

    Updates

---
+++ precache-manifest.5b40909ec3ccb53891280bed1d043d66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13d219b85f2d6e14493ca44ee361773e",
+    "url": "/Novel-Nation/index.html"
+  },
+  {
+    "revision": "a3cf8c04634cd2bdfd24",
+    "url": "/Novel-Nation/static/css/main.708340c5.chunk.css"
+  },
+  {
+    "revision": "7ffd465495bb9a723add",
+    "url": "/Novel-Nation/static/js/2.09f90b01.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Novel-Nation/static/js/2.09f90b01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3cf8c04634cd2bdfd24",
+    "url": "/Novel-Nation/static/js/main.8042242c.chunk.js"
+  },
+  {
+    "revision": "8afc1c6c7d143388d23f",
+    "url": "/Novel-Nation/static/js/runtime-main.80bea1e4.js"
+  }
+]);
